c73e9efb5925a98213367127dfe67886ed228e8e
BarbaraSheaKenny <b.kenny58@gmail.com>
2021-06-12 14:50:55 -0400

    index.js api fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3008;
+const PORT = process.env.PORT || 3002;
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker-bsk',
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
